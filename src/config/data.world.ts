5b71f4c9c8346dfc4b173a2c7d3c43cb83a1f7fc
Maxwell Lincoln <me@maxwelllincoln.com>
2018-04-23 08:07:55 -0500

    fixed typos in config/data.world.ts

--- src/config/data.world.ts
+++ src/config/data.world.ts
@@ -8,8 +8,8 @@ export class DataDotWorldConfig extends Configurable<string, any> {
 
         this.setKeyValue('client_id', process.env.DDW_CLIENT_ID || 'default');
         this.setKeyValue('client_secret', process.env.DDW_CLIENT_SECRET || 'something-secret');
-        this.setKeyValue('connector_uri', process.env.DDW_CLIENT_SECRET || 'localhost');
-        this.setKeyValue('redirect_uri', process.env.DDW_CLIENT_SECRET || 'localhost');
+        this.setKeyValue('connector_uri', process.env.DDW_CONNECTOR_REDIRECT || 'localhost');
+        this.setKeyValue('redirect_uri', process.env.DDW_OAUTH_REDIRECT_URI || 'localhost');
         this.setKeyValue('response_type', process.env.DDW_OAUTH_RESPONSE_TYPE || 'code');
 
     }
