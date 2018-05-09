8b13e75d7638235f8406b85664e08b6ea1fe0c2a
Maxwell Lincoln <me@maxwelllincoln.com>
2018-05-09 14:31:09 -0500

    removed passport strategy

--- src/data.world/passport/index.ts
+++ src/data.world/passport/index.ts
@@ -1,12 +1,6 @@
 import DataDotWorldConfig from '../../config/data.world';
 import DataDotWorldStrategy from './strategy';
 
-const strategy = new DataDotWorldStrategy(
-
-    DataDotWorldConfig.getKeyValue('client_id'),
-    DataDotWorldConfig.getKeyValue('client_secret'),
-    DataDotWorldConfig.getKeyValue('redirect_uri')
-
-);
+const strategy = new DataDotWorldStrategy();
 
 export default strategy;
