1ef77aaa64b1b0ac244c813ca56c9aea473776b5
Maxwell Lincoln <me@maxwelllincoln.com>
2018-07-18 14:51:08 -0500

    refactored webpack configuration so that they would start paying those environment variables forward. #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,109 @@
+const merge = require('webpack-merge');
 const path = require("path");
+const webpack = require('webpack');
 
-module.exports = (env) => ({
-    mode: 'production',
-    entry: {
-        app: "./src/client/index",
-        launch: "./src/client/launch",
-        styles: "./src/client/scss/styles.scss"
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        publicPath: "/",
-        filename: "[name].min.js"
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json", ".scss"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            },
-            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: function () {
-                                return [
-                                    require('precss'),
-                                    require('autoprefixer')
-                                ]
+
+module.exports = (environment) => {
+
+    let common = {
+
+        entry: {
+            app: "./src/client/index",
+            launch: "./src/client/launch",
+            styles: "./src/client/scss/styles.scss"
+        },
+        output: {
+            path: path.resolve(__dirname, "public"),
+            publicPath: "/",
+            filename: "[name].min.js"
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js", ".json", ".scss"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use:  "ts-loader",
+                    exclude: /node_modules/
+                },
+                { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "style-loader"
+                        },
+                        {
+                            loader: "css-loader"
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: function () {
+                                    return [
+                                        require('precss'),
+                                        require('autoprefixer')
+                                    ]
+                                }
                             }
-                        }
 
-                    },
-                    {
-                        loader: "sass-loader"
+                        },
+                        {
+                            loader: "sass-loader"
+                        }
+                    ]
+                },
+                {
+                    test: /\.svg$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "assets/svg/"
                     }
-                ]
-            },
-            {
-                test: /\.svg$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]",
-                    outputPath: "assets/svg/"
+                },
+                {
+                    test: /\.png$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "assets/png/"
+                    }
                 }
-            },
-            {
-                test: /\.png$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]",
-                    outputPath: "assets/png/"
-                }
-            }
+            ]
+        }
+
+
+    };
+
+    common = merge(common, {
+
+        plugins: [
+
+            new webpack.EnvironmentPlugin(['DDW_LTI_CONFIGURATION_URL'])
+
         ]
+
+    });
+
+    if (environment.production) {
+
+        return merge(common, {
+
+            mode: "production",
+            devtool: "source-map"
+
+        });
+
+    } else {
+
+        return merge(common, {
+
+            mode: "development",
+            devtool: "inline-source-map"
+
+        });
+
     }
-});
+
+};
