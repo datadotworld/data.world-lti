7794cd855fd268e0f3a70a38a7923d2b02a39a7f
Maxwell Lincoln <me@maxwelllincoln.com>
2018-07-19 14:17:11 -0500

    optimized webpack config to reduce memory footprint

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,88 @@
-const merge = require('webpack-merge');
 const path = require("path");
 const webpack = require('webpack');
 
 
-module.exports = (environment) => {
+module.exports = {
 
-    let common = {
-
-        entry: {
-            app: "./src/client/index",
-            launch: "./src/client/launch",
-            styles: "./src/client/scss/styles.scss"
-        },
-        output: {
-            path: path.resolve(__dirname, "public"),
-            publicPath: "/",
-            filename: "[name].min.js"
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".js", ".json", ".scss"]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use:  "ts-loader",
-                    exclude: /node_modules/
-                },
-                { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: "style-loader"
-                        },
-                        {
-                            loader: "css-loader"
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                plugins: function () {
-                                    return [
-                                        require('precss'),
-                                        require('autoprefixer')
-                                    ]
-                                }
-                            }
-
-                        },
-                        {
-                            loader: "sass-loader"
-                        }
-                    ]
-                },
-                {
-                    test: /\.svg$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: "assets/svg/"
-                    }
-                },
-                {
-                    test: /\.png$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: "assets/png/"
-                    }
-                }
-            ]
+    mode: "production",
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
         }
+    },
+    plugins: [
 
+        new webpack.EnvironmentPlugin({
 
-    };
+            DDW_LTI_CONFIGURATION_URL: 'http://example.com/lti/condfig'
 
-    common = merge(common, {
+        })
 
-        plugins: [
+    ],
+    entry: {
+        app: "./src/client/index",
+        styles: "./src/client/scss/styles.scss"
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        publicPath: "/",
+        filename: "[name].min.js"
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".json", ".scss"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use:  "ts-loader",
+                exclude: /node_modules/
+            },
+            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
 
-            new webpack.EnvironmentPlugin({
-
-                DDW_LTI_CONFIGURATION_URL: 'http://example.com/lti/condfig'
-
-            })
-
-        ]
-
-    });
-
-    if (process.env.NODE_ENV === 'production') {
-
-        return merge(common, {
-
-            mode: "production",
-            optimization: {
-                splitChunks: {
-                    chunks: 'all'
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: "assets/svg/"
+                }
+            },
+            {
+                test: /\.png$/,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: "assets/png/"
                 }
             }
-
-        });
-
-    } else {
-
-        return merge(common, {
-
-            mode: "development",
-            devtool: "inline-source-map"
-
-        });
-
+        ]
     }
+
 
 };
